8985e88ed34e9ffdac2a65f1ca219b220527f397
r9a <a>
2020-08-08 16:13:11 +0200

    spaghetti button clicking

--- Hive/GUI.py
+++ Hive/GUI.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import math
 from Hive import Hive
+from const import *
 
 hive = Hive()
 
@@ -9,6 +10,10 @@ class GUI():
     def __init__(self):
         window = self.setupWindow()
         board, player1Hand, player2Hand, currentPlayer = hive.newGame()
+        self.handDisplay.updateHandDisplay(player1Hand, player2Hand)
+        availableActions = hive.availableActions(
+            board, player1Hand, currentPlayer)
+        print(availableActions)
         self.drawBoard(board)
         window.mainloop()
 
@@ -57,11 +62,7 @@ class GUI():
         frame = tk.Frame(window)
         frame.pack()
 
-        b1 = tk.Button(frame, width=10)
-        b2 = tk.Button(frame, width=10)
-        b1.pack(side=tk.LEFT)
-        b2.pack(side=tk.RIGHT)
-
+        self.handDisplay = HandDisplay(frame)
         self.setupBoardCanvas(frame)
         return window
 
@@ -82,6 +83,64 @@ class GUI():
         self.canvas.pack()
 
         self.canvas.bind("<Button-1>", self.click)
+
+
+class HandDisplay():
+    def __init__(self, frame):
+        leftFrame = tk.Frame(frame)
+        rightFrame = tk.Frame(frame)
+        leftFrame.pack(side=tk.LEFT)
+        rightFrame.pack(side=tk.RIGHT)
+
+        self.p1aButton = tk.Button(
+            leftFrame, width=10, command=lambda: self.action(Player1A))
+        self.p1gButton = tk.Button(
+            leftFrame, width=10, command=lambda: self.action(Player1G))
+        self.p1sButton = tk.Button(
+            leftFrame, width=10, command=lambda: self.action(Player1S))
+        self.p1bButton = tk.Button(
+            leftFrame, width=10, command=lambda: self.action(Player1B))
+        self.p1qButton = tk.Button(
+            leftFrame, width=10, command=lambda: self.action(Player1Q))
+
+        self.p2aButton = tk.Button(
+            rightFrame, width=10, command=lambda: self.action(Player2A))
+        self.p2gButton = tk.Button(
+            rightFrame, width=10, command=lambda: self.action(Player2G))
+        self.p2sButton = tk.Button(
+            rightFrame, width=10, command=lambda: self.action(Player2S))
+        self.p2bButton = tk.Button(
+            rightFrame, width=10, command=lambda: self.action(Player2B))
+        self.p2qButton = tk.Button(
+            rightFrame, width=10, command=lambda: self.action(Player2Q))
+
+        self.p1aButton.pack(side=tk.TOP)
+        self.p1gButton.pack(side=tk.TOP)
+        self.p1sButton.pack(side=tk.TOP)
+        self.p1bButton.pack(side=tk.TOP)
+        self.p1qButton.pack(side=tk.TOP)
+
+        self.p2aButton.pack(side=tk.TOP)
+        self.p2gButton.pack(side=tk.TOP)
+        self.p2sButton.pack(side=tk.TOP)
+        self.p2bButton.pack(side=tk.TOP)
+        self.p2qButton.pack(side=tk.TOP)
+
+    def updateHandDisplay(self, player1Hand, player2Hand):
+        self.p1aButton['text'] = f'Ant {player1Hand.a}'
+        self.p1gButton['text'] = f'Grass {player1Hand.g}'
+        self.p1sButton['text'] = f'Spider {player1Hand.s}'
+        self.p1bButton['text'] = f'Beetle {player1Hand.b}'
+        self.p1qButton['text'] = f'Queen {player1Hand.q}'
+
+        self.p2aButton['text'] = f'Ant {player2Hand.a}'
+        self.p2gButton['text'] = f'Grass {player2Hand.g}'
+        self.p2sButton['text'] = f'Spider {player2Hand.s}'
+        self.p2bButton['text'] = f'Beetle {player2Hand.b}'
+        self.p2qButton['text'] = f'Queen {player2Hand.q}'
+
+    def action(self, number):
+        print(number)
 
 
 GUI()
--- Hive/Hive.py
+++ Hive/Hive.py
@@ -1,10 +1,17 @@
 import numpy as np
+from const import *
 
 
 class Hive():
     def newGame(self):
         # returns board, player1 hand, player2 hand, currentPlayer
         return np.zeros((23, 23), dtype="int"), Hand(), Hand(), -1
+
+    def availableActions(self, board, playersHand, currentPlayer):
+        avalilableActions = {'place': {}}
+        if np.count_nonzero(board) == 0:
+            avalilableActions['place'] = [Player1Q, (11, 11)]
+        return avalilableActions
 
 
 class Hand():
